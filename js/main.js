a433b4fe2bf0414a36b89432949fa417f59860bf
SBaden <sbaden@design01-ladm.us.nfl.net>
2018-08-22 10:57:44 -0700

    Initial Commit

---
+++ js/main.js
@@ -0,0 +1,41 @@
+(function () {
+    'use strict';
+    alert('JS connected');
+
+    // PASS OBJECT TO JSX FOR EVALUATION
+    var csInterface = new CSInterface();
+    var profileObj = {
+        message: "ExendScript connected"
+    }
+    csInterface.evalScript('parseObj(' + JSON.stringify(profileObj) + ')');
+
+
+    // const replace = require('replace-in-file');
+    // const options = {
+    //     files: '/Users/sbaden/Documents/_TEST_DIRECTORY/EDIT_HIGHLIGHTS/_PROJECTS/PWK01_16_LAC_ARI_EDIT10_BG_TEST02.xml',
+    //     from: '<name>GMF</name>',
+    //     to: '<name>GAMEDAY</name>',
+    // };
+
+    // replace(options)
+    //     .then(changes => {
+    //         console.log('Modified files:', changes.join(', '));
+    //     })
+    //     .catch(error => {
+    //         console.error('Error occurred:', error);
+    //     });  
+
+
+    // TEMP BUTTON FOR DEV: RELOADS EXTENTION PANEL
+    window.onload = function(){
+	    document.getElementById("reloadPanel").addEventListener("click", reloadPanel);
+
+	    function reloadPanel() {
+	        location.reload();
+	    }
+    }
+
+
+
+}());
+    
---
+++ jsx/hostscript.jsx
@@ -0,0 +1,4 @@
+
+function parseObj(obj){
+    alert(obj.message);
+}
