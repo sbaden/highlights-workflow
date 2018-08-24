adce7b2cf653334e581ac6fbd8c64e4d8093671e
Coston <addison.coston@costona-lalm.us.nfl.net>
2018-08-23 18:13:25 -0700

    node works

--- js/main.js
+++ js/main.js
@@ -9,23 +9,23 @@
     // }
     // csInterface.evalScript('parseObj(' + JSON.stringify(profileObj) + ')');
 
-    // NPM PACKAGE 
+    // NPM PACKAGE
     const replace = require('replace-in-file');
-    // const options = {
-    //     files: '/Users/sbaden/Documents/_TEST_DIRECTORY/EDIT_HIGHLIGHTS/_PROJECTS/PWK01_16_LAC_ARI_EDIT10_BG_TEST02.xml',
-    //     from: '<name>GMF</name>',
-    //     to: '<name>GAMEDAY</name>',
-    // };
+    const options = {
+        files: '/Users/addison.coston/Desktop/GDM_WK1_Julio_Jones.xml',//'/Users/sbaden/Documents/_TEST_DIRECTORY/EDIT_HIGHLIGHTS/_PROJECTS/PWK01_16_LAC_ARI_EDIT10_BG_TEST02.xml',
+        from: '<name>GAMEDAY</name>',
+        to: '<name>TOTAL ACCESS</name>',
+    };
 
-    // var contentOfFile = Replace(options)
-    //     .then(changes => {
-    //         // console.log('Modified files:', changes.join(', '));
-    //         alert('Modified files:', changes.join(', '));
-    //     })
-    //     .catch(error => {
-    //         // console.error('Error occurred:', error);
-    //         console.error('Error occurred:', error);
-    //     });  
+    replace(options)
+        .then(changes => {
+            // console.log('Modified files:', changes.join(', '));
+            alert('Modified files:', changes.join(', '));
+        })
+        .catch(error => {
+            // console.error('Error occurred:', error);
+            console.error('Error occurred:', error);
+        });
 
 
     // TEMP BUTTON FOR DEV: RELOADS EXTENTION PANEL
@@ -40,4 +40,3 @@
 
 
 }());
-    
