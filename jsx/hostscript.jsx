d929dbae08bfa85cf37761eb2f9df59325855d5b
SBaden <sbaden@design01-ladm.us.nfl.net>
2018-09-07 12:41:02 -0700

    Export xml working

--- jsx/hostscript.jsx
+++ jsx/hostscript.jsx
@@ -1,17 +1,22 @@
 
-function parseObj(obj){
+// function parseObj(obj){
     // alert(obj.message);
+// }
+
+function sayHello(){
+    alert('hello');
 }
 
 
 function exportXML(targetDirectory){
+    alert('export xml');
     var folder = new Folder (targetDirectory);
     
     if (targetDirectory) {
         var outputName = app.project.name;
-        var extention = .xml;
+        var extention = '.xml';
         var completeOutputPath = targetDirectory + getSep() + outputName + extention;
-        app.project.exportAsFinalCutProXML(completeOutputPath, 1); // 1 == suppress UI
+        app.project.exportFinalCutProXML(completeOutputPath, 1); // 1 == suppress UI
         var info = "Exported FCP XML for " + 
             outputName + 
             " to " + 
