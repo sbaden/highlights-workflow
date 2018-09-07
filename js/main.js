dd7771e4646d77ad3a5e3d8c6afe894c6b31a37f
SBaden <sbaden@design01-ladm.us.nfl.net>
2018-09-07 10:43:49 -0700

    Add jquery

--- js/main.js
+++ js/main.js
@@ -1,67 +1,70 @@
-(function () {
-    'use strict';
+
+$(document).ready(function(){
     alert('JS connected');
 
-    const fs = require('fs');
+    // const fs = require('fs');
 
-    let profile_sot = {
-        xmlDirectory: '/Users/sbaden/Documents/_TEST_DIRECTORY/EDIT_HIGHLIGHTS/_PROJECTS/XML_DOCS/',
-        xml: 'PWK01_16_LAC_ARI_EDIT10_BG_TEST03.xml',
-    }
-
-    $('#btn-version_sot').click(version_sot);
-
-    function version_sot(){
-        alert('clicked');
-        // csInterface.evalScript('exportXML('+ JSON.stringify(profile_sot.xmlDirectory) +')',
-        //     function(result){
-        //         alert(result);
-        //     }
-        // );
-    }
-
-    
-    
-    // let targetXml = profile_sot.xmlDirectory + profile_sot.xml;
-    // let currentShow = 'GMF';
-    // let shows = ['TOTAL_ACCESS', 'GAMEDAY', 'AFTERMATH'];
-
-    // let currentValue1 = new RegExp('<name>'+ currentShow + '<\/name>', 'g');
-    // let currentValue2 = new RegExp('HIGHLIGHTS/'+ currentShow, 'g');
-    // let currentValue3 = new RegExp('<name>'+ currentShow + ' PWK', 'g');
-
-    // let newValue1 = '<name>'+ shows[0] + '</name>';
-    // let newValue2 = 'HIGHLIGHTS/'+ shows[0];
-    // let newValue3 = '<name>'+ shows[0] + ' PWK';
-
-    // const res = fs.readFileSync(targetXml, 'utf-8')
-    //     .replace(currentValue1, newValue1)
-    //     .replace(currentValue2, newValue2)
-    //     .replace(currentValue3, newValue3);
+    var csInterface = new CSInterface();
 
 
-    // fs.writeFile(targetXml, res, 'utf-8', function (err) {
-    //     if (err){
-    //         alert(err);
-    //         return console.log(err);
-    //     }
-    //     else{
-    //         alert('success');
-    //     }
-    // });
-    
-    
-
-
-    // TEMP BUTTON FOR DEV: RELOADS EXTENTION PANEL
     window.onload = function(){
-	    document.getElementById("reloadPanel").addEventListener("click", reloadPanel);
 
-	    function reloadPanel() {
-	        location.reload();
-	    }
+        // const fs = require('fs');
+
+        // let profile_sot = {
+        //     xmlDirectory: '/Users/sbaden/Documents/_TEST_DIRECTORY/EDIT_HIGHLIGHTS/_PROJECTS/XML_DOCS/',
+        //     xml: 'PWK01_16_LAC_ARI_EDIT10_BG_TEST03.xml',
+        // }
+
+        $('#btn-version_sot').on('click', function(){
+            alert('clicked');
+        });
+
+
+        // function version_sot(){
+        //     csInterface.evalScript('exportXML('+ JSON.stringify(profile_sot.xmlDirectory) +')',
+        //         function(result){
+        //             alert(result);
+        //         }
+        //     );
+        // }
+
+        
+        
+        // let targetXml = profile_sot.xmlDirectory + profile_sot.xml;
+        // let currentShow = 'GMF';
+        // let shows = ['TOTAL_ACCESS', 'GAMEDAY', 'AFTERMATH'];
+
+        // let currentValue1 = new RegExp('<name>'+ currentShow + '<\/name>', 'g');
+        // let currentValue2 = new RegExp('HIGHLIGHTS/'+ currentShow, 'g');
+        // let currentValue3 = new RegExp('<name>'+ currentShow + ' PWK', 'g');
+
+        // let newValue1 = '<name>'+ shows[0] + '</name>';
+        // let newValue2 = 'HIGHLIGHTS/'+ shows[0];
+        // let newValue3 = '<name>'+ shows[0] + ' PWK';
+
+        // const res = fs.readFileSync(targetXml, 'utf-8')
+        //     .replace(currentValue1, newValue1)
+        //     .replace(currentValue2, newValue2)
+        //     .replace(currentValue3, newValue3);
+
+
+        // fs.writeFile(targetXml, res, 'utf-8', function (err) {
+        //     if (err){
+        //         alert(err);
+        //         return console.log(err);
+        //     }
+        //     else{
+        //         alert('success');
+        //     }
+        // });
+
+        // TEMP BUTTON FOR DEV: RELOADS EXTENTION PANEL
+        $("#btn_reload").click(reloadPanel);
+
+        function reloadPanel() {
+            location.reload();
+        }
     }
 
-
-
-}());
+});
--- js/main_4.js
+++ js/main_4.js
@@ -1,10 +1,69 @@
-////////  TRY THIS FOR THE XML SEARCH/REPLACE  //////////
+// (function () {
+//     'use strict';
+//     alert('JS connected');
 
-var fs = require('fs');
+//     const fs = require('fs');
 
-var res = fs.readFileSync('/Users/justint/Desktop/test.xml', 'utf-8').replace('show_01', 'show_02');
+//     let profile_sot = {
+//         xmlDirectory: '/Users/sbaden/Documents/_TEST_DIRECTORY/EDIT_HIGHLIGHTS/_PROJECTS/XML_DOCS/',
+//         xml: 'PWK01_16_LAC_ARI_EDIT10_BG_TEST03.xml',
+//     }
 
-fs.writeFile('/Users/justint/Desktop/test.xml', res, 'utf-8', function (err) {
-    if (err)
-        return console.log(err);
-});
+//     $('#btn-version_sot').on('click', function(){
+//         alert('clicked');
+//     });
+
+
+    // function version_sot(){
+    //     csInterface.evalScript('exportXML('+ JSON.stringify(profile_sot.xmlDirectory) +')',
+    //         function(result){
+    //             alert(result);
+    //         }
+    //     );
+    // }
+
+    
+    
+    // let targetXml = profile_sot.xmlDirectory + profile_sot.xml;
+    // let currentShow = 'GMF';
+    // let shows = ['TOTAL_ACCESS', 'GAMEDAY', 'AFTERMATH'];
+
+    // let currentValue1 = new RegExp('<name>'+ currentShow + '<\/name>', 'g');
+    // let currentValue2 = new RegExp('HIGHLIGHTS/'+ currentShow, 'g');
+    // let currentValue3 = new RegExp('<name>'+ currentShow + ' PWK', 'g');
+
+    // let newValue1 = '<name>'+ shows[0] + '</name>';
+    // let newValue2 = 'HIGHLIGHTS/'+ shows[0];
+    // let newValue3 = '<name>'+ shows[0] + ' PWK';
+
+    // const res = fs.readFileSync(targetXml, 'utf-8')
+    //     .replace(currentValue1, newValue1)
+    //     .replace(currentValue2, newValue2)
+    //     .replace(currentValue3, newValue3);
+
+
+    // fs.writeFile(targetXml, res, 'utf-8', function (err) {
+    //     if (err){
+    //         alert(err);
+    //         return console.log(err);
+    //     }
+    //     else{
+    //         alert('success');
+    //     }
+    // });
+    
+    
+
+
+    // TEMP BUTTON FOR DEV: RELOADS EXTENTION PANEL
+//     window.onload = function(){
+// 	    document.getElementById("reloadPanel").addEventListener("click", reloadPanel);
+
+// 	    function reloadPanel() {
+// 	        location.reload();
+// 	    }
+//     }
+
+
+
+// }());
