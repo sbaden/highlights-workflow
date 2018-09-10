47551d6caa8d473fdb8298320dd7407911be6336
SBaden <sbaden@design01-ladm.us.nfl.net>
2018-09-10 11:11:48 -0700

    Callback issue w/import of xml?

--- js/main.js
+++ js/main.js
@@ -2,33 +2,70 @@
 $(document).ready(function(){
     alert('JS connected');
 
+    const fs = require('fs');
     var csInterface = new CSInterface();
-    // csInterface.evalScript('sayHello()');
 
 
     window.onload = function(){
 
-    //     const fs = require('fs');
-
         let profile_sot = {
             xmlDirectory: '/Users/sbaden/Documents/_TEST_DIRECTORY/EDIT_HIGHLIGHTS/_PROJECTS/XML_DOCS/',
-            xml: 'PWK01_16_LAC_ARI_EDIT10_BG_TEST03.xml',
+            targetXml: ''
         }
 
         $('#btn-version_sot').on('click', function(){
-            alert('click');
-
-            version_sot()
-        });
-
-
-        function version_sot(){
             csInterface.evalScript('exportXML('+ JSON.stringify(profile_sot.xmlDirectory) +')',
                 function(result){
-                    alert(result);
+
+                    profile_sot.targetXml = result;
+                    // alert(profile_sot.targetXml);
+
+                    
+                    //////////////  CALLBACK FUNCTION /////////////////
+                    ///////////////////////////////////////////////////
+                    let currentShow = 'TOTAL_ACCESS';
+                    let shows = ['GMF', 'GAMEDAY', 'AFTERMATH'];
+
+                    let currentValue1 = new RegExp('<name>'+ currentShow + '<\/name>', 'g');
+                    let currentValue2 = new RegExp('HIGHLIGHTS/'+ currentShow, 'g');
+                    let currentValue3 = new RegExp('<name>'+ currentShow + ' PWK', 'g');
+
+                    let newValue1 = '<name>'+ shows[0] + '</name>';
+                    let newValue2 = 'HIGHLIGHTS/'+ shows[0];
+                    let newValue3 = '<name>'+ shows[0] + ' PWK';
+
+                    const res = fs.readFileSync(profile_sot.targetXml, 'utf-8')
+                        .replace(currentValue1, newValue1)
+                        .replace(currentValue2, newValue2)
+                        .replace(currentValue3, newValue3);
+
+
+                    fs.writeFile(profile_sot.targetXml, res, 'utf-8', function (err) {
+                        if (err){
+                            alert(err);
+                            return console.log(err);
+                        }
+                        else{
+                            alert('search & replace success');
+                        }
+                    });
+                    ///////////////////////////////////////////////////
+                    ///////////////////////////////////////////////////
+
+
+
+                    //// IMPORT RESULTING XML - AS CALLBACK FUNCTION
+                    //// IMPORT MIGHT BE ERRORING BECAUSE IT IS IMPORTING BEFORE XML HAS BEEN EDITED - FIGURE OUT CALLBACK ////
+                    csInterface.evalScript('importXML('+ JSON.stringify(profile_sot.targetXml) +')',
+                        function(result){
+                            if(result){
+                                // alert(result);
+                            }
+                        }
+                    );
                 }
             );
-        }
+        });
 
 
         
--- jsx/hostscript.jsx
+++ jsx/hostscript.jsx
@@ -9,11 +9,13 @@ function sayHello(){
 
 
 function exportXML(targetDirectory){
-    alert('export xml');
     var folder = new Folder (targetDirectory);
     
     if (targetDirectory) {
         var outputName = app.project.name;
+        outputName = outputName.substr(0, outputName.lastIndexOf('.'));
+        alert('output name: ' + outputName);
+    
         var extention = '.xml';
         var completeOutputPath = targetDirectory + getSep() + outputName + extention;
         app.project.exportFinalCutProXML(completeOutputPath, 1); // 1 == suppress UI
@@ -23,6 +25,7 @@ function exportXML(targetDirectory){
             completeOutputPath + 
             ".";
         // $.writeln(info);
+        alert(info);
 
         return completeOutputPath;
 
@@ -30,10 +33,38 @@ function exportXML(targetDirectory){
     else { $.writeln("XML Directory can not be found."); }
 }
 
+function importXML(targetFile){
+    var result = app.project.importFiles([targetFile], 1); // 1 == suppress UI
+    return result;
+}
+    
+
 function getSep() {
 	if (Folder.fs == 'Macintosh') {
 		return '/';
 	} else {
 		return '\\';
 	}
+}
+
+
+
+
+
+
+//// INCORPORATE INTO THE REFRESH PANEL BUTTON TO RELOAD JSX
+function loadJSX() {
+	var csInterface = new CSInterface();
+
+	// get the appName of the currently used app. For Premiere Pro it's "PPRO"
+	var appName = csInterface.hostEnvironment.appName;
+	var extensionPath = csInterface.getSystemPath(SystemPath.EXTENSION);
+
+	// load general JSX script independent of appName
+	var extensionRootGeneral = extensionPath + '/jsx/';
+	csInterface.evalScript('$._ext.evalFiles("' + extensionRootGeneral + '")');
+
+	// load JSX scripts based on appName
+	var extensionRootApp = extensionPath + '/jsx/' + appName + '/';
+	csInterface.evalScript('$._ext.evalFiles("' + extensionRootApp + '")');
 }
